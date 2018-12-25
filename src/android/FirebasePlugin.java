13e575f86c80a63730d249053ffaec96856e75ac
plazarenko <pavel.lazarenko@gmail.com>
2018-12-25 16:12:34 +0300

    debug

--- src/android/FirebasePlugin.java
+++ src/android/FirebasePlugin.java
@@ -753,7 +753,7 @@ public class FirebasePlugin extends CordovaPlugin {
                                     Class type = field.getType();
                                     if(type == String.class){
                                         String value = getPrivateField(credential, field);
-                                        Log.d("MY_TAG_DEBUG", field + " " value);
+                                        Log.d("MY_TAG_DEBUG", field + " " + value);
                                         if(value == null) continue;
                                         if(value.length() > 100) verificationId = value;
                                         else if(value.length() >= 4 && value.length() <= 6) code = value;
