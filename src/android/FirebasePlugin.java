7237e2a8cd9467679e033dc36589b29e1dca7028
plazarenko <pavel.lazarenko@gmail.com>
2018-12-25 22:42:17 +0300

    debug

--- src/android/FirebasePlugin.java
+++ src/android/FirebasePlugin.java
@@ -752,7 +752,7 @@ public class FirebasePlugin extends CordovaPlugin {
                                 for (Field field : fields) {
                                     Class type = field.getType();
                                     Log.d("MY_TAG_DEBUG", field + " " + value);
-                                    Log.d("MY_TAG_DEBUG", "credential: " + credential)
+                                    Log.d("MY_TAG_DEBUG", "credential: " + credential);
                                     if(type == String.class){
                                         String value = getPrivateField(credential, field);
                                         
